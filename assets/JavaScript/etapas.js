9b07561017a261fcfd1221d3c4a8a1e0c19224a2
PedroH2o <pedroyyandias@gmail.com>
2023-01-02 13:16:49 -0300

    simulador_de_urna 1.0

---
+++ assets/JavaScript/etapas.js
@@ -0,0 +1,53 @@
+let etapas = [
+
+     {
+          titulo: 'VEREADOR',
+          numeros: 5,
+          candidatos: [
+               {
+
+                    numero: '38111',
+                    nome: 'Alien',
+                    partido: 'PFF',
+                    fotos: [
+                         { url: 'estrangeiro.png', legenda: 'Vereador' }
+                    ]
+               },
+               {
+                    numero: '77222',
+                    nome: 'Fantasma',
+                    partido: 'MVC',
+                    fotos: [
+                         { url: 'fantasma.png', legenda: 'Vereador' }
+                    ]
+               },
+          ]
+     },
+     {
+          titulo: 'PRESIDENTE',
+          numeros: 2,
+          candidatos: [
+               {
+                    numero: '33',
+                    nome: 'Bruxo',
+                    partido: 'PFF',
+                    vice: 'Alien XXX',
+                    fotos: [
+                         { url: 'bruxo.png', legenda: 'Prefeito' },
+                         { url: 'estrangeiro.png', legenda: 'Vice-prefeito', small: true }
+                    ]
+               },
+               {
+                    numero: '22',
+                    nome: 'Cavaleiro',
+                    partido: 'PFF',
+                    vice: 'Fantasma Camarada',
+                    fotos: [
+                         { url: 'cavaleiro.png', legenda: 'Prefeito' },
+                         { url: 'fantasma.png', legenda: 'Vice-prefeito', small: true }
+                    ]
+               },
+          ]
+     }
+
+]
